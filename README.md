86b053ca6dc758fd186c4861f01776b3fe6bcc7e
Ondřej Tůma <ondr@centrum.cz>
2020-06-11 16:25:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Node.js script to list all branches 
+and pick the ones to delete
+
+## Toolkit
+
+* inquirer
+* simple-git
---
+++ index.js
@@ -0,0 +1,41 @@
+// Node.js
+
+(async () => {
+    const simpleGit = require('simple-git')
+    const inquirer = require('inquirer')
+
+    const args = process.argv
+    const workingDir = args[2] || './'
+
+    const git = simpleGit(workingDir)
+
+    const questions = []
+
+    try {
+        const { all } = await git.branchLocal()
+
+        questions.push({
+            type: 'checkbox',
+            name: 'branchesToDelete',
+            message: 'Which branches you want to delete?',
+            choices: all,
+        })
+        questions.push({
+            type: 'confirm',
+            name: 'confirm',
+            message: 'Are you sure?'
+        })
+
+        const prompt = inquirer.createPromptModule()
+
+        const {branchesToDelete, confirm} = await prompt(questions)
+
+        if (!confirm) {
+            return
+        }
+
+        await git.deleteLocalBranches(branchesToDelete)
+    } catch (e) {
+        console.error(e)
+    }
+})()
